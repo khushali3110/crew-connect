167ea8c8263c3a96b13110ecc3cd6f1bcdb1aeed
khushali jikadra <jikadrakhsuhali@gmail.com>
2026-06-20 21:02:00 +0530

    Use MongoDB Atlas env variable

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const { default: mongoose } = require("mongoose");
 
 const dbConfig = mongoose
-  .connect(process.env.DB_URL)
+  .connect(process.env.MONGO_URL)
   .then(() => console.log("db connected 👍"))
   .catch((err) => console.log(err));
 
